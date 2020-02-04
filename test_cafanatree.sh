5e10471632fb30f27771c0d36542add1c9440fe5
Tanaz Mohayai <tmohayai@users.noreply.github.com>
2020-02-04 11:27:53 -0600

    Added Option for Larger-scale Production

--- test_cafanatree.sh
+++ test_cafanatree.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
+#if using the skimmer approach
 bin/mcp_skimmer --infile anatree_100.root --outfile skimmed.root --debug 1
 echo "done with running skimming module"
 bin/cafanatree_module --infile skimmed.root --outfile caf.root
 echo "done with running cafanatree module"
+
+#if attemting a larger-scale sample production
+bin/cafanatree_module --infile anatree.root --outfile caf.root
