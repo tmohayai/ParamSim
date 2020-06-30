138711d6e328971b64360cd230b6afc4c6dfecb9
ebrianne <eldwan.brianne@desy.de>
2020-06-30 10:27:11 +0200

    Correct problem in vector length, added check for vector length, close properly rootfile at end program:

--- include/CAF.h
+++ include/CAF.h
@@ -42,6 +42,9 @@ public:
     /* Method to loop */
     void loop();
 
+    /* Method to check vector size */
+    void CheckVectorSize();
+
 private:
 
     TFile * cafFile; ///< The output TFile pointer */
@@ -54,24 +57,24 @@ private:
 
     std::string _inputfile;
     std::string _outputFile; ///< The output TFile name */
-    int _correct4origin;     ///< sets the string for the coordinates origins (World or TPC)
+    unsigned int _correct4origin;     ///< sets the string for the coordinates origins (World or TPC)
 
     //Event-wise values
-    int _Run, _Event, _SubRun;
+    unsigned int _Run, _Event, _SubRun;
     //Generator values
     std::vector<int> mode, ccnc, ntype, gint, weight, tgtpdg, gt_t, intert, detected;
     std::vector<double> q2, w, y, x, theta, t, mctime, mcnupx, mcnupy, mcnupz, vertx, verty, vertz;
     //MC Particle Values, with motherid added
-    std::vector<int> _nFSP;
+    std::vector<unsigned int> _nFSP;
     std::vector<int> mctrkid, motherid, pdgmother, truepdg, _MCPStartX, _MCPStartY, _MCPStartZ, _MCPEndX, _MCPEndY, _MCPEndZ;
     std::vector<std::string> _MCProc, _MCEndProc;
-    std::vector<float> trkLen, trkLenPerp, truep, truepx, truepy, truepz, _angle;
+    std::vector<double> trkLen, trkLenPerp, truep, truepx, truepy, truepz, _angle;
     //Reco values
     std::vector<int> recopid, recopidecal;
-    std::vector<float> prob_arr, partereco, anglereco, _preco, erecon, etime;
+    std::vector<double> prob_arr, partereco, anglereco, _preco, erecon, etime;
     //Geometry
-    std::vector<int> isFidStart, isTPCStart, isCaloStart, isThroughCaloStart;
-    std::vector<int> isFidEnd, isTPCEnd, isCaloEnd, isThroughCaloEnd;
+    std::vector<unsigned int> isFidStart, isTPCStart, isCaloStart, isThroughCaloStart;
+    std::vector<unsigned int> isFidEnd, isTPCEnd, isCaloEnd, isThroughCaloEnd;
 };
 
 #endif
