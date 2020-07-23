9110d82db86ccbb3b7b501a117ac29f10251936a
ebrianne <eldwan.brianne@desy.de>
2020-07-23 09:49:54 +0200

    Updated to accomodate genie v3

--- .travis-ci.d/setup_env.sh
+++ .travis-ci.d/setup_env.sh
@@ -3,4 +3,5 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 
 setup cmake v3_14_3
-setup genie v2_12_10c  -q e17:debug
+genie_xsec     	v3_00_04a -q e19:prof
+genie_phyopt    v3_00_04 -q e19:prof
