ebab45e0b8e9c69b10e2bc2ea2b0a9d19d7237f0
ebrianne <eldwan.brianne@desy.de>
2020-07-23 09:58:47 +0200

    Fix setup genie for travis

--- .travis-ci.d/setup_env.sh
+++ .travis-ci.d/setup_env.sh
@@ -3,5 +3,4 @@
 source /cvmfs/dune.opensciencegrid.org/products/dune/setup_dune.sh
 
 setup cmake v3_14_3
-genie_xsec     	v3_00_04a -q e19:prof
-genie_phyopt    v3_00_04 -q e19:prof
+setup genie v3_00_06i -q e19:prof
